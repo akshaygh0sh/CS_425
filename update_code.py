c7c9990f44aa9111f279ec6f79169c92d1c7c16a
gdurand2 <gdurand2@illinois.edu>
2023-09-21 20:43:24 -0500

    parralized update

--- update_code.py
+++ update_code.py
@@ -1,6 +1,9 @@
-import paramiko
-from github import Github
+import paramiko 
+import concurrent.futures
 import getpass
+# SSH functions 
+
+
 
 MACHINE_LIST = [
     "fa23-cs425-5601.cs.illinois.edu",
@@ -14,41 +17,51 @@ MACHINE_LIST = [
     "fa23-cs425-5609.cs.illinois.edu",
     "fa23-cs425-5610.cs.illinois.edu"
 ]
+def connect_ssh(hostname, username, password):
+  # Connect to hostname
+    print(username)
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(hostname, username=username, password=password)
+    client.exec_command(f"git config --global user.email {username}@illinois.edu", get_pty=True)
+    client.exec_command(f"git config --global user.name {username}", get_pty=True)
+    client.exec_command("git config --global --unset https.proxy", get_pty=True)
+    client.exec_command("git config --global --unset http.proxy", get_pty=True)
+    git_clone_command = f"rm -rf CS_425 ; git clone https://{username}:{access_token}@gitlab.engr.illinois.edu/gdurand2/CS_425.git"
+    stdin, stdout, stderr, = client.exec_command(git_clone_command, get_pty=True)
+    for line in iter(stdout.readline, ""):
+        print(line, end="")
+    print('finished.')
+    print(f"Machine: {hostname}\n{stdout.read().decode()}")
+    client.close()
+    
 
-# Clone code on each machine
-def git_pull_remote(to_connect):
+def git_clone(client, username, access_token):
+  # Clone repo on client
+    client.exec_command(f"git config --global user.email {username}@illinois.edu", get_pty=True)
+    client.exec_command(f"git config --global user.name {username}", get_pty=True)
+    client.exec_command("git config --global --unset https.proxy", get_pty=True)
+    client.exec_command("git config --global --unset http.proxy", get_pty=True)
+    git_clone_command = f"rm -rf CS_425 ; git clone https://{username}:{access_token}@gitlab.engr.illinois.edu/gdurand2/CS_425.git"
+    stdin, stdout, stderr, = client.exec_command(git_clone_command, get_pty=True)
+    for line in iter(stdout.readline, ""):
+        print(line, end="")
+    print('finished.')
+    #print(f"Machine: {hostname}\n{stdout.read().decode()}")
+    client.close()
+# Parallel execution
+
+
+    
+    
+    
+def main():
     username = input("Enter your username: ")
     password = getpass.getpass("Enter your password: ")
     access_token = input("Enter your access_token: ")
-    ssh_client = paramiko.SSHClient()
-    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    for machine_ix in to_connect:
-        hostname = machine_ix
-        try:
-            ssh_client.connect(
-                hostname=hostname,
-                port= 22,
-                username=username,
-                password=password,
-            )
-            # Delete folder and then clone
-            ssh_client.exec_command(f"git config --global user.email {username}@illinois.edu", get_pty=True)
-            ssh_client.exec_command(f"git config --global user.name {username}", get_pty=True)
-            ssh_client.exec_command("git config --global --unset https.proxy", get_pty=True)
-            ssh_client.exec_command("git config --global --unset http.proxy", get_pty=True)
-            git_clone_command = f"rm -rf cs425_mp1 ; git clone https://{username}:{access_token}@gitlab.engr.illinois.edu/gdurand2/cs425_mp1"
-            stdin, stdout, stderr, = ssh_client.exec_command(git_clone_command, get_pty=True)
-            for line in iter(stdout.readline, ""):
-                print(line, end="")
-            print('finished.')
-            print(f"Machine: {hostname}\n{stdout.read().decode()}")
-            ssh_client.close()
-        except Exception as e:
-            print(e)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=len(MACHINE_LIST)) as executor:
 
-
-def main():
-    git_pull_remote(MACHINE_LIST)
-
+        results = [executor.submit(connect_ssh, h, username, password) for h in MACHINE_LIST]
+    
 if __name__ == "__main__":
     main()
