64d426ea6ace2f23b2496a042447fabfcb553aeb
gdurand2 <gdurand2@illinois.edu>
2023-11-04 22:40:20 -0500

    hello

--- cs_425_mp3/node.py
+++ cs_425_mp3/node.py
@@ -187,7 +187,7 @@ class Server:
                                 self.file_info[file_key] = data_list[file_key]
                                 #print("2")
                             
-                            elif data_list[file_key]['heartbeat'] > self.file_info[file_key]['hearbeat']: #this statment fails
+                            elif ('heartbeat' in data_list[file_key] and 'heartbeat' in self.file_info[file_key]) and data_list[file_key]['heartbeat'] > self.file_info[file_key]['hearbeat']: #this statment fails
                                 #print("4")
                                 self.file_info[file_key]['hearbeat'] = data_list[file_key]['heartbeat']
                                 #print("5")
