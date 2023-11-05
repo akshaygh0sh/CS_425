ac674c1ab56f18562d3e23eeb847e4a93f2e2d50
aaghosh2 <aaghosh2@illinois.edu>
2023-11-05 01:47:14 -0500

    Fix

--- cs_425_mp3/node.py
+++ cs_425_mp3/node.py
@@ -199,7 +199,7 @@ class Server:
                             if (len(overlap) > 1):
                                 # Nodes that got nominated to replicate that don't have file yet
                                 no_file = list(set(new_locations)- overlap)
-                                self.handle_replica_replacement(random.sample(overlap, 1)[0], no_file)
+                                self.handle_replica_replacement(file_key, random.sample(overlap, 1)[0], no_file)
 
                             self.file_info[file_key]["locations"] =  new_locations
                     except Exception as e:
