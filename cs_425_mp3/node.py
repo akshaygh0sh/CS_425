45178050506df397fec27172ff2a20dc22dbc99a
aaghosh2 <aaghosh2@illinois.edu>
2023-11-05 19:47:50 -0600

    Fix membershpi list

--- cs_425_mp3/node.py
+++ cs_425_mp3/node.py
@@ -709,7 +709,7 @@ class Server:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-t', '--protocol-period', type=float, help='Protocol period T in seconds', default=0.25)
+    parser.add_argument('-t', '--protocol-period', type=float, help='Protocol period T in seconds', default=0.1)
     parser.add_argument('-d', '--drop-rate', type=float,
                         help='The message drop rate',
                         default=0)
