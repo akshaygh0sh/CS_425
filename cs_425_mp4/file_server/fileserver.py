6369a434c69e26d596ec608499b9beedc5fe7ca1
aaghosh2 <aaghosh2@illinois.edu>
2023-12-03 03:11:37 -0600

    Fix

--- cs_425_mp4/file_server/fileserver.py
+++ cs_425_mp4/file_server/fileserver.py
@@ -574,7 +574,7 @@ def handleMapleRequest(http_packet):
     command = [maple_exe, f"sharded_{map_file}"]
     try:
         result = subprocess.run(command, check = True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        keys_json = result.stdout
+        keys_json = result.stdout.decode()
         response_packet = {}
         response_packet['request_type'] = 'maple_response'
         response_packet['maple_results'] = keys_json
